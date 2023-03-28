b81cdbbf10c6f0a71a496b1c1f19bd979555936c
Jatin pandey <jatin26pandey@gmail.com>
2023-03-28 17:00:39 +0530

    frontend req

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -10,7 +10,7 @@ const Orders = () => {
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () =>
-      newRequest.get('/orders', { withCredentials: true }).then(res => { return res.data }).catch(err => {
+      newRequest.post('/orders', {}, { withCredentials: true }).then(res => { return res.data }).catch(err => {
         console.log(err);
       })
 
